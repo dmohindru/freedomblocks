38911f5e9d0c246a0a53348d55df5c10ed75766f
Dhruv Mohindru <dmohindru@gmail.com>
2017-02-10 09:15:58 +0530

    Home PC 10.02.07

--- src/resources.h
+++ src/resources.h
@@ -1,12 +1,12 @@
 #ifndef RESOURCES_H
 #define RESOURCES_H
 
-#define GAME_BACKGROUND 	"../graphics/background.png" //Play Background file
-#define GAME_DATA			    "../graphics/data.png" //Graphics data file
-#define WELCOME_BACKGROUND  "../graphics/welcome.png" //Welcome Background file
-#define TUTORIAL_BACKGROUND "../graphics/tutorial.png" //Tutorial Background file
-#define MESSAGES_BACKGROUND "../graphics/messages.png" //Messages data file
-#define GAMEMUSIC           "../music/Tetris_theme.ogg" //Tetris music file
+#define GAME_BACKGROUND 	"/home/dhruv/Programming/freedomblocks/graphics/background.png" //Play Background file
+#define GAME_DATA			    "/home/dhruv/Programming/freedomblocks/graphics/data.png" //Graphics data file
+#define WELCOME_BACKGROUND  "/home/dhruv/Programming/freedomblocks/graphics/welcome.png" //Welcome Background file
+#define TUTORIAL_BACKGROUND "/home/dhruv/Programming/freedomblocks/graphics/tutorial.png" //Tutorial Background file
+#define MESSAGES_BACKGROUND "/home/dhruv/Programming/freedomblocks/graphics/messages.png" //Messages data file
+#define GAMEMUSIC           "/home/dhruv/Programming/freedomblocks/music/Tetris_theme.ogg" //Tetris music file
 
 #define COLORKEY_RED        0 //Color key red component
 #define COLORKEY_GREEN      0 //Color key green component
