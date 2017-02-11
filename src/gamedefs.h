65649f096673d38e2135bf26766ba24552cf0cb5
Dhruv Mohindru <dhruv@dhruv-desktop.(none)>
2017-02-11 19:20:35 +0530

    Office PC 11.02.17

--- src/gamedefs.h
+++ src/gamedefs.h
@@ -36,13 +36,13 @@ SDL_Surface *temp; //temp surface storing game data
 #define TRUE										1 //basic stuff
 
 #define NEXT_LEVEL_LINES				10 //Number of lines for next level
-#define NEXT_LEVEL_TIME					200 //Time in milliseconds for next level difficulty
+#define NEXT_LEVEL_TIME					150 //Time in milliseconds for next level difficulty
 #define DELAY_START							1500 //Initial Delay for block to fall
-#define LANDED_DELAY						400 //delay before to check for landed position
+#define LANDED_DELAY						300 //delay before to check for landed position
 #define SCORES_PER_LINE					10 //Scores per line
 #define SCORES_BONUS						5	//Bonus scores for recussive line completion
 
-#define MAX_LEVELS							50 //Max Levels to call for win
+#define MAX_LEVELS							10 //Max Levels to call for win
 #define LEVEL_LINES							10 //Lines to clear a level
 
 #define GAME_OVER_STATE					1 //Game over state
